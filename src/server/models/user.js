8e3780f27bc616dfe119bfcb90c79920e5310d20
Meeshbhoombah <rohanmishra2017@gmail.com>
2019-05-08 10:51:50 -0700

    Started user model

---
+++ src/server/models/user.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, DataTypes) => {
+  var User = sequelize.define('User', {
+    id: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+      allowNull: false,
+      unique: {
+        name: 'team_id',
+        msg: 'A Team with this ID already exists.',
+      }
+    },
+    token: { 
+      type: DataTypes.STRING, 
+      allowNull: false
+    },
+    name: DataTypes.STRING,
+    chanId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    chanName: DataTypes.STRING, 
+  }, {
+    tableName: 'users',
+    underscored: true,
+  });
+
+  return User
+}
