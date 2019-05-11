7412ed653aff1d983c1e5cbca447424dbdf22689
Meeshbhoombah <rohanmishra2017@gmail.com>
2019-05-11 12:44:09 -0700

    Fixed oauth integrate promise error

--- src/server/controllers/oauth.js
+++ src/server/controllers/oauth.js
@@ -16,10 +16,15 @@ async function authorizeTeam(clientId, clientSecret, perms) {
 async function integrateTeam(config, db, accessCode, clientId, clientSecret) {
   const baseUri = 'https://slack.com/app_redirect?' //channel=%s&team=%s
 
-  return new Promise(async (resolve, reject) => {
+  return new Promise((resolve, reject) => {
     let Team = db.Team;
 
-    await slack.oauth.access(clientId, clientSecret, accessCode) 
+    slack.oauth.access({
+      client_id: clientId, 
+      client_secret: clientSecret, 
+      code: accessCode,
+      nice: 1
+    }) 
     .then((res) => {
       console.log(Team);
       console.log(res);
